74e023adce0cbf3214abbb71e0b72faba3c3f71f
dvanhout89 <123621238+dvanhout89@users.noreply.github.com>
2023-04-13 13:37:01 +0200

    Updated SideNav component.
    Updated the SideNav component, cleaned up code.

--- Client/src/app/sidenav/sidenav.component.ts
+++ Client/src/app/sidenav/sidenav.component.ts
@@ -1,50 +1,3 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-sidenav',
-//   templateUrl: './sidenav.component.html',
-//   styleUrls: ['./sidenav.component.less']
-// })
-// export class SidenavComponent {
-//   events: string[] = [];
-// }
-// export class SidenavOpenCloseExample {
-//   events: string[] = [];
-//   opened: boolean | undefined;
-// }
-// export class AppComponent {
-//   events: string[] = [];
-//   opened: boolean | undefined;
-
-//   constructor() {}
-
-//   toggle(sidenav: MatSidenav) {
-//     sidenav.toggle();
-//     this.events.push(this.opened ? 'open!' : 'close!');
-//   }
-// }
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-sidenav',
-//   templateUrl: './sidenav.component.html',
-//   styleUrls: ['./sidenav.component.less']
-// })
-// export class SideNavComponent {
-//   menuItems = [
-//     { name: 'Dashboard', link: '/dashboard' },
-//     { name: 'Customers', link: '/customers' },
-//     { name: 'Products', link: '/products' },
-//     { name: 'Estimates', link: '/estimates' },
-//     { name: 'Invoices', link: '/invoices' }
-//   ];
-//   isOpen = false;
-
-//   toggleMenu() {
-//     this.isOpen = !this.isOpen;
-//   }
-// }
-
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
@@ -55,8 +8,8 @@ import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/l
   styleUrls: ['./sidenav.component.less']
 })
 export class SideNavComponent {
+  
   isHandset$: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
 
   constructor(private breakpointObserver: BreakpointObserver) {}
-
 }
