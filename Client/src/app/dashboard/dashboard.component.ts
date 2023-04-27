5463617ee599e656fdeb783a4a44561a3d420afc
dvanhout89 <123621238+dvanhout89@users.noreply.github.com>
2023-04-27 18:17:36 +0200

    Added Dashboard pages and Navigation routes.
    Added Dashboard pages and Navigation routes. Added basic page layout. Removed products page and route, will add again later.

--- Client/src/app/dashboard/dashboard.component.ts
+++ Client/src/app/dashboard/dashboard.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.less']
+  styleUrls: ['./dashboard.component.less'],
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
---
+++ Client/src/app/pc-builder/pc-builder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pc-builder',
+  templateUrl: './pc-builder.component.html',
+  styleUrls: ['./pc-builder.component.less']
+})
+export class PcBuilderComponent {
+
+}
