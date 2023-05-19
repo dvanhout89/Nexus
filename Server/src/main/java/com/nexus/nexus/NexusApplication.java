74e27956a07d332510383283477e374cd27cfe61
dvanhout89 <123621238+dvanhout89@users.noreply.github.com>
2023-05-19 14:36:22 +0200

    Updated Products and product list component
    Updated Products and product list component

--- Server/src/main/java/com/nexus/nexus/NexusApplication.java
+++ Server/src/main/java/com/nexus/nexus/NexusApplication.java
@@ -2,12 +2,9 @@ package com.nexus.nexus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class NexusApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NexusApplication.class, args);
 	}
-
 }
