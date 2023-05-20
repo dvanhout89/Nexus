0ebacc5c2293dfde5c86098fb97e6638f631df9b
dvanhout89 <123621238+dvanhout89@users.noreply.github.com>
2023-05-20 12:28:19 +0200

    Updated WebConfig.
    Updated and Fixed WebConfig for CORS issues.

--- Server/src/main/java/com/nexus/nexus/config/WebConfig.java
+++ Server/src/main/java/com/nexus/nexus/config/WebConfig.java
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    // Override the addCorsMappings method to add CORS configurations
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") //All endpoints
-                .allowedOrigins("http://localhost:4200") //Allow this origin. Port:4200
-                .allowedMethods("GET", "POST", "PUT", "DELETE")//Allowed Methods
-                .allowedHeaders("*") //Allow all headers
-                .allowCredentials(true);
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowedHeaders("Content-Type", "Date", "Total-Count", "loginInfo")
+                .exposedHeaders("Content-Type", "Date", "Total-Count", "loginInfo")
+                .maxAge(3600);
     }
 }
