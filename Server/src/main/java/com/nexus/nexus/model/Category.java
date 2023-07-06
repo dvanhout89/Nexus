5308e66668e9f9b4d66277b15160f1ef41f1b7ff
dvanhout89 <123621238+dvanhout89@users.noreply.github.com>
2023-07-06 15:10:17 +0200

    Updated Category Model.
    Added Constructors, getters, setters for the Category model.

--- Server/src/main/java/com/nexus/nexus/model/Category.java
+++ Server/src/main/java/com/nexus/nexus/model/Category.java
@@ -1,13 +1,67 @@
 package com.nexus.nexus.model;
 import jakarta.persistence.*;
 
-
 @Entity
 @Table(name = "categories")
 public class Category {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
-    //Constructors, getters, and setters...
+
+    @Column
+    private String description;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    // Constructors, getters, and setters
+
+    public Category() {
+    }
+
+    public Category(String name, String description, String imageUrl) {
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    // Other methods
 }
